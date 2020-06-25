48bfd47c3e109a17caf711beb73bdbef54b5e8a2
Olle Harju <11613018+ollhar@users.noreply.github.com>
2020-06-25 21:04:59 +0200

    feat: remove gfp configs

--- rules/es6.js
+++ rules/es6.js
@@ -1,16 +1,4 @@
 module.exports = {
-    rules: {
-        'arrow-spacing': [2, { 'before': true, 'after': true }],
-        'no-const-assign': 2,
-        'no-duplicate-imports': 2,
-        'no-useless-computed-key': 2,
-        'no-useless-rename': 2,
-        'no-var': 2,
-        'prefer-const': 2,
-        'prefer-template': 2,
-        'rest-spread-spacing': 2,
-        'sort-imports': 2,
-        'template-curly-spacing': 2,
-    },
+    rules: {},
     globals: {}
 };
